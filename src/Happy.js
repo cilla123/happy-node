2ff938c7aad92171d0475148915ce0b3b288d2d6
Ben <892156860@qq.com>
2018-03-06 21:25:49 +0800

    增加中间件的读取

--- src/Happy.js
+++ src/Happy.js
@@ -4,14 +4,24 @@ const Emitter = require('events')
 class Happy extends Emitter {
 
     constructor(config){
-        super()
-        this.app = new Koa()
+      super()
+      this.app = new Koa()
+    }
+
+    /** 
+     * 使用中间件
+    */
+    useMiddlewares(){
+      R.map(R.compose(R.forEachObjIndexed(initWith => initWith(app)),
+          require,
+          name => resolve(__dirname, `./middlewares/${name}`)
+      ))(MIDDLEWARES)
     }
 
     run(){
-        this.app.listen(8080, () => {
-            console.log('启动成功')
-        })
+      this.app.listen(8080, () => {
+          console.log('启动成功')
+      })
     }
 
 }
